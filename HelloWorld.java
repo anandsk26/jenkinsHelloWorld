e9f8d0f43f274c5a762cfeb8c9f07f7181d0dbdb
anandsk26 <anandsk26@gmail.com>
2020-11-28 11:46:39 -0500

    Adding code to git for jenkin

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package net.codejava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("-----****Hellow world and welcome to jenkin training***----");
+		// TODO Auto-generated method stub
+
+	}
+
+}
